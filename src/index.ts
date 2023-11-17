90acd0833e380817cfb0c4e48912541a09caf4c3
EdamAme-x <edame8080@gmail.com>
2023-11-17 19:15:44 +0900

    add graph

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,23 @@ import fs from "node:fs";
 
 const app = new Hono()
 app.get('/', async (c) => {
-    const html = await fs.readFileSync("../static/idnex.html", {
+    const html = await fs.readFileSync("./static/index.html", {
         encoding: "utf-8"
     });
 
     return c.html(html);
+})
+
+app.get("/runtime", async (c) => {
+    const js = fs.readFileSync("./static/index.js", {
+        encoding: "utf-8"
+    });
+
+    return new Response(js, {
+        headers: new Headers({
+            "Content-Type": "text/javascript"
+        })
+    })
 })
 
 serve(app)
--- static/index.js
+++ static/index.js
@@ -1,1 +1,8 @@
 console.log("Function Guess / By @amex2189");
+
+window.addEventListener("load", (event)=>{
+	const jsx = JXG.JSXGraph.initBoard("jxgbox", 
+		{axis: true, boundingbox: [-15, 15, 15, -15]});
+
+    jsx.create("functiongraph", [x => x*2], {strokeColor:"purple"});
+});
